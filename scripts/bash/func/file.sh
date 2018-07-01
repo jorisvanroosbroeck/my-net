e6a04396b96ac790b704ab5a18caee4bb1161839
Joris Van Roosbroeck <jvr@mng.my-net.lan>
2018-07-01 16:03:18 +0200

    My-Net - FrameWork v.01

---
+++ scripts/bash/func/file.sh
@@ -0,0 +1,32 @@
+# =============
+# INFO: file.sh
+# =============
+
+
+# =================
+# FUNCTION: file.sh
+# =================
+function file.line.change {
+    FILE=$1
+    SEARCHSTR=$2
+    REPLACESTR=$3
+
+    sed -i "s/$SEARCHSTR/$REPLACESTR/g" $FILE
+}
+
+function file.line.check {
+    CHECKFILE=$1
+    CHECKSTRING=$2
+
+    unset CHECK
+    CHECK=`cat $CHECKFILE | grep -P "(^|\s)\K$CHECKSTRING(?=\s|$)"`
+
+    if [ ${#CHECK} -gt 0 ]
+    then
+        echo "---------- $CHECKSTRING --------- OK"
+    fi
+    if [ ${#CHECK} == 0 ]
+    then
+        echo "---------- $CHECKSTRING --------- Not OK"
+    fi
+}
