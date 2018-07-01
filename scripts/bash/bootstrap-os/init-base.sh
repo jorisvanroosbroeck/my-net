06643320803e957b581115ac436630fb24d01a22
Joris Van Roosbroeck <jvr@mng.my-net.lan>
2018-07-01 14:28:52 +0200

    My-Net - Tweaking

--- scripts/bash/bootstrap-os/init-base.sh
+++ scripts/bash/bootstrap-os/init-base.sh
@@ -6,13 +6,17 @@ source "$HOME/my-net/env-vars/mn-vars.sh"
 
 # FUNCTIONS
 # =========
+source "$HOME/my-net/scripts/bash/func/confUpdateSystem.sh"
+source "$HOME/my-net/scripts/bash/func/instBaseTools.sh"
+source "$HOME/my-net/scripts/bash/func/confSSH.sh"
+
 function rebootHost {
     sudo shutdown -r now
 }
 
 # PROGRAM
 # =======
-source "$HOME/my-net/scripts/bash/func/confUpdateSystem.sh"
-source "$HOME/my-net/scripts/bash/func/instBaseTools.sh"
-source "$HOME/my-net/scripts/bash/func/confSSH.sh"
+confUpdateSystem
+instBaseTools
+confSSH
 #rebootHost
