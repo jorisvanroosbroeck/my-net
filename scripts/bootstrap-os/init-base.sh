aae082347452154440fb39a38592e7eca1aa5faa
Joris Van Roosbroeck <jvr@mng.my-net.lan>
2018-07-01 12:40:13 +0200

    init-base.sh - Tweaking

--- scripts/bootstrap-os/init-base.sh
+++ scripts/bootstrap-os/init-base.sh
@@ -24,6 +24,7 @@ function installBaseTools {
 
 function configSSH {
     # Add SSH public key
+    touch .ssh/authorized_keys
     echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDNYLhu/zjnPWyw6XRJokLhnfmkiJQl9JpZM7eThtGFPreOpNHAIkpIysQ1BZDIwjEIAKF6TT1Kzethx0Ux+yxo6PNz8PXWhOKd2pXEk0GwmqN7TuN614wWBs2sCPu99yqyRx1C/m8z6vA+ehiWYPJD/gJYLygz7B/bfVw6yNviOF9KBE/RxFdQTxD19CPpbXBt6THIn7opRMiHFm/NhxSPqTzU89OPa0TlbEzA96VcI1fN4XH9U3+nazW5qvXlBRBEExBmlgYFr9fAWGFvVDWl01nvqKUVrodTaXoISOW1juB8/+yFNusoeIq4rqKCdGMc7b6+ndl2LT4dv5+t6nGj farique@temp-01" > .ssh/authorized_keys
 
     # Config SSH Port
