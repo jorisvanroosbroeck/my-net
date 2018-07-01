accb5d95a5c0c89cc3ebb1bb8e62fc99dfd44f4c
Joris Van Roosbroeck <jvr@mng.my-net.lan>
2018-07-01 16:25:48 +0200

    My-Net - FrameWork v.01

--- scripts/bash/func/ssh.sh
+++ scripts/bash/func/ssh.sh
@@ -2,6 +2,7 @@
 # INFO: SSH.sh
 # ============
 # $SSH_PUBLIC_KEY = Mandatory in calling script!
+# $SSH_PORT = Mandatory in calling script!
 # my-net/scripts/func/file.sh = Mandatory in calling script!
 
 # ================
@@ -24,7 +25,7 @@ function ssh.server.conf {
     # Config SSH Port
     FILE='/etc/ssh/sshd_config'
     SEARCHSTR="#Port 22"
-    REPLACESTR="Port 65132"
+    REPLACESTR="Port $SSH_PORT"
     file.line.change "$FILE" "$SEARCHSTR" "$REPLACESTR"
     file.line.check "$FILE" "$REPLACESTR"
 
