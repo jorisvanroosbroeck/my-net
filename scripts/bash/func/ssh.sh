3a87a074962373d7db401f6cd466f619fe60274c
Joris Van Roosbroeck <jvr@mng.my-net.lan>
2018-07-01 19:51:06 +0200

    My-Net - FrameWork v0.1

--- scripts/bash/func/ssh.sh
+++ scripts/bash/func/ssh.sh
@@ -19,6 +19,7 @@ function ssh.client.conf {
         touch "$HOME/.ssh/authorized_keys"
     fi
     echo "$SSH_PUBLIC_KEY" > "$HOME/.ssh/authorized_keys"
+    file.line.check "$HOME/.ssh/authorized_keys" "$SSH_PUBLIC_KEY"
 }
 
 function ssh.server.conf {
