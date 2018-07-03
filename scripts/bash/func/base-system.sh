b1356f0815819a6dcf543e3eb6c447891a4b3264
Joris Van Roosbroeck <jvr@mng.my-net.lan>
2018-07-04 00:45:27 +0200

    My-Net - FrameWork v.01

--- scripts/bash/func/base-system.sh
+++ scripts/bash/func/base-system.sh
@@ -26,11 +26,31 @@ function base.system.clean {
 }
 
 function base.system.net.ip.dhcp {
-    #
+    FILE=$1
+    MSG=$2
+    IFACE=$3
+
+    sed -i "$ a\# My-Net: $IFACE - $MSG" $FILE
+    sed -i "$ a\auto $IFACE" $FILE
+    sed -i "$ a\iface $IFACE inet dhcp"
 }
 
 function base.system.net.ip.static {
-    #
+    FILE=$1
+    MSG=$2
+    IFACE=$3
+    IP=$4
+    GW=$5
+    DNS=$6
+    SRCH=$7
+
+    sed -i "$ a\# My-Net: $IFACE - $MSG" $FILE
+    sed -i "$ a\auto $IFACE" $FILE
+    sed -i "$ a\iface $IFACE inet static" $FILE
+    sed -i "$ a\t\addresst\ $IP" $FILE
+    sed -i "$ a\t\gatewayt\ $GW" $FILE
+    sed -i "$ a\t\nameservert\ $DNS" $FILE
+    sed -i "$ a\t\searcht\ $SRCH" $FILE
 }
 
 function base.system.net.bridge.inst {
@@ -39,11 +59,14 @@ function base.system.net.bridge.inst {
 }
 
 function base.system.net.bridge.ip.dhcp {
-    #
+    base.system.net.ip.dhcp "$1" "$2" "$3"
 }
 
 function base.system.net.bridge.ip.static {
-    #
+    base.system.net.ip.static "$1" "$2" "$3" "$4" "$5" "$6" "$7"
+    sed -i "$ a\t\t\blablabla" $1
+    sed -i "$ a\t\t\blablabla" $1
+    sed -i "$ a\t\t\blablabla" $1
 }
 
 # ========================
